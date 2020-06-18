1d7001828b78ff1d8b9c4277fbe251a26058f831
wuchuheng <wuchuheng@163.com>
2020-06-18 23:12:18 +0800

    截图下载

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,38 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>hello world</h1>
+    <button @click="downloadCapture2Png()">保存为图片</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import html2canvas from 'html2canvas';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  methods: {
+    //下载截图
+    downloadCapture2Png() {
+      html2canvas(document.body).then(function(canvas) {
+        let uri = canvas.toDataURL()
+        let filename = 'canvas.png'
+        var link = document.createElement('a');
+        if (typeof link.download === 'string') {
+          link.href = uri;
+          link.download = filename;
+          //Firefox requires the link to be in the body
+          document.body.appendChild(link);
+          //simulate click
+          link.click();
+          //remove the link when done
+          document.body.removeChild(link);
+        } else {
+          window.open(uri);
+        }
+      });
+    }
   }
 }
 </script>
